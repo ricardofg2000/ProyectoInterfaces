300a9bf8957aa45da3f2467e6c8731f6ca9a20c2
Ricardo <Ricardo@192.168.1.22>
2023-03-08 21:11:26 +0100

    Documentación de la clase CompNombreAgrupacion

--- src/model/CompNombreAgrupacion.java
+++ src/model/CompNombreAgrupacion.java
@@ -2,8 +2,19 @@ package model;
 
 import java.util.Comparator;
 
+/**
+ * Clase CompNombreAgrupacion se utiliza para comparar Objetos del tipo
+ * Agrupación según el nombre de la agrupación
+ */
 public class CompNombreAgrupacion implements Comparator<Agrupacion> {
 
+	/**
+	 * Compara dos objetos del tipo Agrupación segun el nombre
+	 *
+	 * @param o1 <i>Primer Objeto Agrupación</i>
+	 * @param o2 <i>Segundo Objeto Agrupación</i>
+	 * @return Resultado de la comparación
+	 */
 	@Override
 	public int compare(Agrupacion o1, Agrupacion o2) {
 		int comp = 0;
