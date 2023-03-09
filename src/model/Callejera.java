4ff0cf8e26179a2f6db5cdf44899289af6734377
Ricardo <Ricardo@192.168.1.22>
2023-03-09 21:35:05 +0100

    Documentación de la Intefaz Callejera del paquete model

--- src/model/Callejera.java
+++ src/model/Callejera.java
@@ -1,6 +1,15 @@
 package model;
 
+/**
+ * Interfaz Callejera, implementa una serie de métodos utilizados por algunas 
+ * clases del tipo AgrupaciónOficial
+ */
 public interface Callejera {
 
+	/**
+	 * Genera un String compuesto por texto y el nombre de la Agrupación
+	 *
+	 * @return String amoAescucha
+	 */
 	public abstract String amoAescucha();
 }
