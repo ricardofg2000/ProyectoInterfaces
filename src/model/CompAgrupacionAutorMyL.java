fb21a0056acca571ec87d0e33446b0b6e6be22b9
Ricardo <Ricardo@192.168.1.22>
2023-03-09 21:07:10 +0100

    Documentación de la clase CompAgrupacionAutorMyL del paquete model

--- src/model/CompAgrupacionAutorMyL.java
+++ src/model/CompAgrupacionAutorMyL.java
@@ -2,8 +2,22 @@ package model;
 
 import java.util.Comparator;
 
+/**
+ * Clase CompAgrupacionAutorMyL se utiliza para ordenar Objetos del tipo
+ * Agrupación según el nombre del autor de la musica y del nombre del 
+ * autor de la letra
+ */
 public class CompAgrupacionAutorMyL implements Comparator<Agrupacion> {
 
+	/**
+	 * Compara dos objetos del tipo Agrupación según el nombre 
+	 * del autor de la musica y del nombre del 
+	 * autor de la letra
+	 *
+	 * @param o1 <i>Primer Objeto Agrupación</i>
+	 * @param o2 <i>Segundo Objeto Agrupación</i>
+	 * @return int Resultado de la comparación
+	 */
 	@Override
 	public int compare(Agrupacion o1, Agrupacion o2) {
 		int comp = 0;
