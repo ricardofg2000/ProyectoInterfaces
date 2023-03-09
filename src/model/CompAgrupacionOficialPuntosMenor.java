b0c90fe6d82df28e4943d0c123f25587e7c01a2b
Ricardo <Ricardo@192.168.1.22>
2023-03-09 21:00:30 +0100

    Documentación clase CompNombreAgrupacion del paquete model

--- src/model/CompAgrupacionOficialPuntosMenor.java
+++ src/model/CompAgrupacionOficialPuntosMenor.java
@@ -2,8 +2,19 @@ package model;
 
 import java.util.Comparator;
 
+/**
+ * Clase CompNombreAgrupacion se utiliza para comparar Objetos del tipo
+ * AgrupaciónOficial según el número de puntos de la AgrupaciónOficial
+ */
 public class CompAgrupacionOficialPuntosMenor implements Comparator<AgrupacionOficial>{
 
+	/**
+	 * Compara dos objetos del tipo AgrupaciónOfical según el número puntos
+	 *
+	 * @param o1 <i>Primer Objeto Agrupación</i>
+	 * @param o2 <i>Segundo Objeto Agrupación</i>
+	 * @return int Resultado de la comparación
+	 */
 	@Override
 	public int compare(AgrupacionOficial o1, AgrupacionOficial o2) {
 		int comp = 0;
